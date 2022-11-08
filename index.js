39d8b5dd995e01ca92e1e98e6bfd0701ffac400a
Sazeduzzaman Saju <sazeduzzaman@yahoo.com>
2022-11-08 07:14:20 +0600

    Mongodb Database Connected

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kystxht.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const servicesCollection = client.db('photo-serve').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(error => console.error(error))
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Volunteer Api Server is Running')
@@ -13,3 +41,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Volunteer server running in ${port} `);
 })
+
+
